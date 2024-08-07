95bcb6369912e5c7f9278c873e01eea50a1f5585
maruki00 <aitbaha48@gmail.com>
2024-08-07 08:49:35 +0300

    --

--- invoicer/invoicer.pb.go
+++ invoicer/invoicer.pb.go
@@ -4,7 +4,7 @@
 // 	protoc        v3.21.12
 // source: proto/invoicer.proto
 
-package grpc
+package invoicer
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-	"go/grpc/invoicer/go/grpc"
+	"context"
+	"go/grpc/invoicer"
 	"log"
 	"net"
+
+	"google.golang.org/grpc"
 )
+
+type MyInvoice struct {
+	invoicer.UnimplementedInvoicerServer
+}
+
+func (o *MyInvoice) Create(context context.Context, _ *invoicer.CreateRequest) (*invoicer.CreateResponse, error) {
+	return &invoicer.CreateResponse{
+		Pdf:  []byte("test"),
+		Docx: []byte("test"),
+	}, nil
+}
 
 func main() {
 	listner, err := net.Listen("tcp", "8089")
@@ -13,5 +27,8 @@ func main() {
 	}
 
 	serverRigister := grpc.NewServer()
-	grpc.Invoicer.RegisterInvoicerServer(serverRigister)
+	service := &MyInvoice{}
+	invoicer.RegisterInvoicerServer(serverRigister, service)
+
+	serverRigister.Serve(listner)
 }
